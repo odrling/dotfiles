bd16201228f76990d3401411721d93475e707af5
odrling <odrling@gmail.com>
2018-02-01 23:34:24 +0100

    initial commit setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+alias dots="git --git-dir=$HOME/.dots --work-tree=$HOME"
+
+dots init && \
+dots remote add origin https://git.odrling.xyz/odrling/dotfiles.git && \
+dots fetch origin master && \
+dots checkout -b master --track origin/master && \
+dots submodule init ~/.zsh && \
+dots submodule update
