a70b8c34e6868f51c1773ad12c8d3758bb6ba6f0
odrling <florianbadie@odrling.xyz>
2018-09-14 01:33:00 +0200

    dots command uses HOME

--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,4 +1,3 @@
-# Defined in - @ line 0
-function dots --description 'alias dots=git --git-dir=/home/odrling/.dots --work-tree=/home/odrling'
-	git --git-dir=/home/odrling/.dots --work-tree=/home/odrling $argv;
+function dots
+	git --git-dir="$HOME/.dots" --work-tree="$HOME" $argv;
 end
