0e5909fcac3f5b1906f2719f5bf4fd5112917ae2
odrling <florianbadie@odrling.xyz>
2019-01-30 09:42:14 +0000

    init vim submodules

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ dots remote add origin https://git.odrling.xyz/odrling/dotfiles.git && \
 dots fetch origin master && \
 dots checkout -b master --track origin/master && \
 dots submodule init ~/.zsh && \
+dots submodule init ~/.vim && \
 dots submodule update
