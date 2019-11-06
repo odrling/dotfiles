85b38bf822443acb899b8eef4272c680f9460cb3
odrling <florianbadie@odrling.xyz>
2019-11-06 09:55:05 +0100

    move .profile and .zshrc during setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ alias dots="git --git-dir=$HOME/.dots --work-tree=$HOME"
 dots init "$HOME"
 dots remote add origin https://git.odrling.xyz/odrling/dotfiles.git
 dots fetch origin master
+[ -f "$HOME/.profile" ] && mv -vi "$HOME/.profile" "$HOME/.profile.local"
+[ -f "$HOME/.zshrc" ] && mv -vi "$HOME/.zshrc" "$HOME/.zshrc.local"
 dots checkout -b master --track origin/master
 dots submodule update --init --recursive
