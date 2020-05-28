eae62e368806b1132216352030aac67aad1b30af
odrling <florianbadie@odrling.xyz>
2020-05-28 19:37:50 +0200

    [mpv] remove check that doesn't work for redshift

--- .config/mpv/scripts/caffeine.lua
+++ .config/mpv/scripts/caffeine.lua
@@ -3,21 +3,19 @@ local utils = require 'mp.utils'
 
 local function start_caffeine()
     local caffeine_command = {}
+    local redshift_command = {}
+
     caffeine_command.args = {"caffeine", "enable"}
-
-    if mp.get_property('video-codec') ~= nil then
-        local redshift_command = {}
-        redshift_command.args = {"control_redshift", "disable"}
-        utils.subprocess_detached(redshift_command)
-    end
-
+    redshift_command.args = {"control_redshift", "disable"}
+    utils.subprocess_detached(redshift_command)
     utils.subprocess_detached(caffeine_command)
 end
 
 local function stop_caffeine()
     local caffeine_command = {}
-    caffeine_command.args = {"caffeine", "disable"}
     local redshift_command = {}
+
+    caffeine_command.args = {"caffeine", "disable"}
     redshift_command.args = {"control_redshift", "enable"}
 
     utils.subprocess_detached(caffeine_command)
