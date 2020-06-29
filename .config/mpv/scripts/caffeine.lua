c4633c6ce0ec2fcb8219b47034804cd7422c7c87
odrling <florianbadie@odrling.xyz>
2020-06-29 18:51:33 +0200

    [mpv] move redshift to its own script

--- .config/mpv/scripts/caffeine.lua
+++ .config/mpv/scripts/caffeine.lua
@@ -1,14 +1,11 @@
--- caffeine script - don't lock the screen and disable redshift when mpv is open
-local utils = require 'mp.utils'
+-- caffeine script - don't lock the screen when mpv is open
 
 local function start_caffeine()
     mp.commandv("run", "caffeine", "enable")
-    mp.commandv("run", "control_redshift", "disable")
 end
 
 local function stop_caffeine()
     mp.commandv("run", "caffeine", "disable")
-    mp.commandv("run", "control_redshift", "enable")
 end
 
 mp.register_event('shutdown', stop_caffeine)
---
+++ .config/mpv/scripts/redshift.lua
@@ -0,0 +1,17 @@
+-- redshift.lua - disable redshift while mpv is playing a video
+
+local function control_redshift(name, value)
+   if value then
+      mp.commandv("run", "control_redshift", "disable")
+   else
+      mp.commandv("run", "control_redshift", "enable")
+   end
+end
+
+local function restart_redshift()
+   mp.commandv("run", "control_redshift", "enable")
+end
+
+mp.observe_property("vo-configured", "bool", control_redshift)
+mp.register_event("shutdown", restart_redshift)
+control_redshift("vo-configured", mp.get_property("vo-configured", true))
