18e7b4cbcdf43c788e2a6913e249fb96adf1e6d8
odrling <florianbadie@odrling.xyz>
2020-12-25 00:00:51 +0100

    [mpv] configuration file for catch_up

--- .mpv/scripts/catch_up.lua
+++ .mpv/scripts/catch_up.lua
@@ -1,12 +1,22 @@
 
+settings = {
+
+   speed_up = 1.10,
+   catchup_until = 3,
+
+}
+
+local opts = require 'mp.options'
+opts.read_options(settings, "catch_up")
+
+
 local catch_up = false
-local speed_up = 1.10
 
 
 function stop_catchup()
    local remaining = mp.get_property("demuxer-cache-time") - mp.get_property("playback-time")
 
-   if remaining < 2 then
+   if remaining < settings.catchup_until then
       mp.set_property("speed", 1.00)
       catch_up = false
    else
@@ -19,7 +29,7 @@ function catchup()
    catch_up = not catch_up
 
    if catch_up then
-      mp.set_property("speed", speed_up)
+      mp.set_property("speed", settings.speed_up)
       mp.set_property("pause", "no")
       mp.add_timeout(2, stop_catchup)
    else
