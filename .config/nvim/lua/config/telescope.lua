f940b785dd0174f22a8109bb68cde00a98dc0a13
odrling <florianbadie@odrling.xyz>
2022-06-14 15:38:23 +0200

    [nvim] change default fd command

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,12 +1,10 @@
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+  pickers = {
+    find_files = {
+      find_command = { "fd", "--type", "f", "--strip-cwd-prefix", "--hidden" }
     },
+  },
+  extensions = {
     ["ui-select"] = {
       require("telescope.themes").get_dropdown {
       }
