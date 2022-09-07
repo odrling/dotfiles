7061619ccec5d0bf09ce374c54880911c0b43a57
odrling <florianbadie@odrling.xyz>
2022-09-07 09:02:12 +0200

    setup with codeberg host

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 alias dots="git --git-dir=$HOME/.dots --work-tree=$HOME"
 
 dots init "$HOME"
-dots remote add origin https://git.odrling.xyz/odrling/dotfiles.git
+dots remote add origin https://codeberg.org/odrling/dotfiles.git
 dots fetch origin master
 [ -f "$HOME/.profile" ] && mv -vi "$HOME/.profile" "$HOME/.profile.local"
 [ -f "$HOME/.zshrc" ] && mv -vi "$HOME/.zshrc" "$HOME/.zshrc.local"
