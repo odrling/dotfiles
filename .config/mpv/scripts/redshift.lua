fc56b5bf4de74b478031a7172f6990d04a6ee65d
odrling <florianbadie@odrling.xyz>
2022-09-25 01:27:10 +0200

    [mpv] redshift: format

--- .config/mpv/scripts/redshift.lua
+++ .config/mpv/scripts/redshift.lua
@@ -1,15 +1,15 @@
 -- redshift.lua - disable redshift while mpv is playing a video
 
 local function control_redshift(name, value)
-   if value then
-      mp.commandv("run", "control_redshift", "disable")
-   else
-      mp.commandv("run", "control_redshift", "enable")
-   end
+	if value then
+		mp.commandv("run", "control_redshift", "disable")
+	else
+		mp.commandv("run", "control_redshift", "enable")
+	end
 end
 
 local function restart_redshift()
-   mp.commandv("run", "control_redshift", "enable")
+	mp.commandv("run", "control_redshift", "enable")
 end
 
 mp.observe_property("vo-configured", "bool", control_redshift)
