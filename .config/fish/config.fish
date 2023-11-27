eceeafbc407384b19469310fa2848ea3c34b5b9c
odrling <florianbadie@odrling.xyz>
2023-11-27 19:36:55 +0100

    [fish] remove old dots alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,6 @@
 
 fish_vi_key_bindings 2> /dev/null # bind: Key with name “btab” does not have any mapping
 
-alias dots="git --git-dir=$HOME/.dots --work-tree=$HOME"
-
 for init in ~/.config/fish/modules/*/init.fish
     source "$init"
 end
