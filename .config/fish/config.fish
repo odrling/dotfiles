1c17a3e87cff9b911607ecfc12f2dd0e21ac4d2e
odrling <florianbadie@odrling.xyz>
2023-11-27 19:36:22 +0100

    [fish] add OSC-7 config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,14 @@ for init in ~/.config/fish/modules/*/init.fish
     source "$init"
 end
 
+# OSC-7
+function update_cwd_osc --on-variable PWD --description 'Notify terminals when $PWD changes'
+    if status --is-command-substitution || set -q INSIDE_EMACS
+        return
+    end
+    printf \e\]7\;file://%s%s\e\\ $hostname (string escape --style=url $PWD)
+end
+
+update_cwd_osc # Run once since we might have inherited PWD from a parent shell
+
 starship init fish | source
