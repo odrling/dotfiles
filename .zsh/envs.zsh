5bfc477dbd8d25c06cbc77b037412cd9db927d15
odrling <florianbadie@odrling.xyz>
2024-03-12 19:43:58 +0100

    [zsh] load envrc from parent dirs before current envrc

--- .zsh/envs.zsh
+++ .zsh/envs.zsh
@@ -86,6 +86,8 @@ function odr-debug() {
 }
 
 local loadenvrc() {
+    [ "$1" != "/" ] && loadenvrc "$(dirname "$1")"
+
     if [ -f "$1/.envrc" ]; then
         einfo "loading $1/.envrc"
         cat "$1/.envrc" >&2
