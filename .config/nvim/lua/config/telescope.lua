6fc175644840b23229e8c66ad1a1983a2de67c62
odrling <florianbadie@odrling.xyz>
2025-12-15 15:29:36 +0100

    [nvim] show hidden files in telescope

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,4 +1,8 @@
 require('telescope').setup({
+    pickers = {
+        -- show hidden files
+        hidden = true,
+    },
     defaults = {
         layout_strategy = "vertical",
         generic_sorter = require('mini.fuzzy').get_telescope_sorter,
