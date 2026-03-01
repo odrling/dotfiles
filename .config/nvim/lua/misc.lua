050f440afc2e251b974a878fa6041bf1e64d2a2c
odrling <florianbadie@odrling.xyz>
2026-03-01 02:32:49 +0100

    [nvim] completely disable gentoo augroup
    none of the gentoo autocmds are particularly useful in my setup so it
    can all be ignored

--- .config/nvim/lua/misc.lua
+++ .config/nvim/lua/misc.lua
@@ -24,8 +24,8 @@ vim.o.backup = false
 vim.o.swapfile = false
 vim.o.writebackup = false
 
--- don’t let the default gentoo autocmd do its thing as it breaks gitcommit
-vim.g.leave_my_cursor_position_alone = true
+-- don’t let the default gentoo autocmds do their things
+vim.api.nvim_del_augroup_by_name("gentoo")
 
 -- use system vim syntaxes/ftdetect
 vim.opt.rtp:append('/usr/share/vim/vimfiles')
