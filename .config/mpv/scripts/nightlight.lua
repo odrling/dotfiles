5371332bc147157bfd66700196780565425477b3
odrling <florianbadie@odrling.xyz>
2026-07-09 05:37:12 +0200

    [mpv] support controlling noctalia nightlight setting

--- .config/mpv/scripts/nightlight.lua
+++ .config/mpv/scripts/nightlight.lua
@@ -1,49 +1,58 @@
--- wlsunset.lua - disable wlsunset while mpv is playing a video
+-- nightlight.lua - disable nightlight while mpv is playing a video
 -- could use USR1 but I find it not as easy to use for this purpose
 local settings = {
     on_focus = false,
     on_vo_configuration = true,
+    backend = 'noctalia'
 }
 
 local opts = require("mp.options")
 opts.read_options(settings, "wlsunset")
 
 local manual = false
-local wlsunset_state
+local nightlight_state
 
 local function reset_manual()
     manual = false
 
-    wlsunset_state = (
-        on_focus and mp.get_property("focused") and
-        on_vo_configuration and mp.get_property("vo-configured")
+    nightlight_state = (
+        settings.on_focus and mp.get_property("focused") and
+        settings.on_vo_configuration and mp.get_property("vo-configured")
     )
     update_wlsunset()
 end
 
 local function update_wlsunset()
-    if wlsunset_state then
-        mp.commandv("run", "rc-service", "-U", "wlsunset", "stop")
+    if nightlight_state then
+        if settings.backend == "noctalia" then
+            mp.commandv("run", "noctalia", "msg", "nightlight-disable")
+        elseif settings.backend == "wlsunset" then
+            mp.commandv("run", "rc-service", "-U", "wlsunset", "stop")
+        end
     else
-        mp.commandv("run", "rc-service", "-U", "wlsunset", "start")
+        if settings.backend == "noctalia" then
+            mp.commandv("run", "noctalia", "msg", "nightlight-enable")
+        elseif settings.backend == "wlsunset" then
+            mp.commandv("run", "rc-service", "-U", "wlsunset", "start")
+        end
     end
 end
 
 local function toggle_wlsunset()
     manual = true
-    wlsunset_state = not wlsunset_state
+    nightlight_state = not nightlight_state
     update_wlsunset()
 end
 
 local function control_wlsunset(name, value)
     if not manual then
-        wlsunset_state = value
+        nightlight_state = value
         update_wlsunset()
     end
 end
 
 local function restart_wlsunset()
-    wlsunset_state = false
+    nightlight_state = false
     update_wlsunset()
 end
 
